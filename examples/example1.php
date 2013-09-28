8eb192741f952baff27773f1a715c54b0cf2f861
Mark Gannaway <mark@ganners.co.uk>
2013-09-28 23:15:10 +0100

    Added some styling to the example

--- examples/example1.php
+++ examples/example1.php
@@ -2,20 +2,33 @@
 <html>
     <head>
         <title>HTML Pixels</title>
+        <style>
+            body {
+                zoom: 1;
+                text-align: center;
+            }
+            .container {
+                margin: 0 auto;
+                display: inline-block;
+                width: auto;
+            }
+        </style>
     </head>
 
     <body>
-    <?php
+        <div class="container">
+            <?php
 
-    // Grab our class
-    require_once(realpath('../src/ImagePixelDrawer.php'));
+            // Grab our class
+            require_once(realpath('../src/ImagePixelDrawer.php'));
 
-    // Construct, passing the location of our image
-    $pixelDrawer = new \experiment\ImagePixelDrawer(realpath('images/test.jpg'));
+            // Construct, passing the location of our image
+            $pixelDrawer = new \experiment\ImagePixelDrawer(realpath('images/test.jpg'));
 
-    // Prints out the image in HTML
-    echo $pixelDrawer->getPixelHTML();
+            // Prints out the image in HTML
+            echo $pixelDrawer->getPixelHTML();
 
-    ?>
+            ?>
+        </div>
     </body>
 </html>
